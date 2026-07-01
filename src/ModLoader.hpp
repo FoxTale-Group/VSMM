caff60c2fa57df8c7396599dc67b2ecd66c47e4e
Karol Szuster <karolsz9898@gmail.com>
2026-07-02 01:45:45 +0200

    chore(backend): update copyrights headers
    Signed-off-by: Karol Szuster <karolsz9898@gmail.com>

--- src/ModLoader.hpp
+++ src/ModLoader.hpp
@@ -1,6 +1,6 @@
 /*
- * VS Mod Manager - A mod management tool for Vintage Story
- * Copyright (C) 2026 Amaroq & StardustVulpine
+ * VSMM - A mod management tool for Vintage Story
+ * Copyright (C) 2026 FoxTale-Group VSMM Team
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
